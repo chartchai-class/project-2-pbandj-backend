858d9b04a7b592271eeb3f654554f60b6c91f49f
techatha <techatham_p@cmu.ac.th>
2024-10-24 21:17:10 +0700

    init

---
+++ src/main/java/se331/pbandjbackend/PbandjBackendApplication.java
@@ -0,0 +1,13 @@
+package se331.pbandjbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbandjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbandjBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se331/pbandjbackend/PbandjBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se331.pbandjbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbandjBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
